c8dc67e31045024fb274db1ef1ec59f31794577b
lyndsayp <lyndsayp@gmail.com>
2023-01-17 09:10:36 +0000

    Cpp version upgrade plus include headers to make EXPECT_THAT work.

--- Google_tests/TemplateCppTests.cpp
+++ Google_tests/TemplateCppTests.cpp
@@ -1,7 +1,11 @@
 #include "gtest/gtest.h"
 #include "TemplateCppClass.h"
+#include "lib/googlemock/include/gmock/gmock-matchers.h"
+
+using ::testing::HasSubstr;
 
 TEST(TemplateCppTestSuite, Example){
 
-ASSERT_EQ(TemplateCppClass::GetSomething(),0);
+    ASSERT_EQ(TemplateCppClass::GetSomething(),0);
+    EXPECT_THAT("somthing", HasSubstr("ome"));
 }
